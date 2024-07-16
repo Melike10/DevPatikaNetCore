11f6bbc5929ce325b73b04674c0889aa984ea67a
GÖZ'SS <GZ'SS@DESKTOP-NKTDPLS>
2024-07-16 12:24:15 +0300

    Add project files.

---
+++ deneme/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Melike Demir Göz");
+            Console.WriteLine("Yazılım dünyasında Patika'nın uzman kadrosunun da yardımyla daha bilinçli, temiz ve dinamik kodlar yazan bir yazılımcı olmayı istediğim için bu bootcampteyim.");
+
+        }
+    }
+}
